b87705b2dd79a012f66e52cb165f5f6d35acdaf0
Emmanuel Debanne <e.debanne@criteo.com>
2014-11-26 10:37:08 +0100

    Add logs when loading rules
    I think it can be useful to trace that we are loading the ndepend rules
    when starting Sonar.
    
    Change-Id: I083838de804d2c8b6830aaa8c6b54c421d5e5657

--- src/main/java/org/sonar/plugins/ndepend/NdependRulesDefinition.java
+++ src/main/java/org/sonar/plugins/ndepend/NdependRulesDefinition.java
@@ -20,12 +20,16 @@ package org.sonar.plugins.ndepend;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.api.server.rule.RulesDefinitionXmlLoader;
 
 import com.google.common.base.Charsets;
 
 public class NdependRulesDefinition implements RulesDefinition {
+  private static final Logger LOG = LoggerFactory
+      .getLogger(NdependRulesDefinition.class);
   public static final String RULES_RESOURCE = "/org/sonar/plugins/ndepend/rules.xml";
   private final RulesDefinitionXmlLoader xmlLoader;
 
@@ -35,10 +39,12 @@ public class NdependRulesDefinition implements RulesDefinition {
 
   @Override
   public void define(Context context) {
+    LOG.info("Defining rules for NDepend...");
     NewRepository repository = context.createRepository(NdependConfig.REPOSITORY_KEY, NdependConfig.LANGUAGE_KEY);
     repository.setName("NDepend - C#");
     InputStream stream = getClass().getResourceAsStream(RULES_RESOURCE);
     xmlLoader.load(repository, new InputStreamReader(stream, Charsets.UTF_8));
     repository.done();
+    LOG.info("{} rules loaded.", repository.rules().size());
   }
 }
